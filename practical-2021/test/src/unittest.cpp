91ed4e88e65a631ad9da72b464ad231eca92371a
Luise <ufziw@student.kit.edu>
2021-04-19 18:23:36 +0200

    Make tests run at build

--- practical-2021/test/src/unittest.cpp
+++ practical-2021/test/src/unittest.cpp
@@ -1,5 +1,10 @@
 #include "gtest/gtest.h"
 
+int main(int argc, char* argv[]) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
+
 TEST(testMath, myCubeTest)
 {
     EXPECT_EQ(1, 1);
