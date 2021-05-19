3908e1234ee6eaa48465a11127a0baeadaaf1c11
Luise <ufziw@student.kit.edu>
2021-05-19 09:06:47 +0200

    Add ortools to project

--- practical-2021/test/src/gtest/GeneralizedRFTest.cpp
+++ practical-2021/test/src/gtest/GeneralizedRFTest.cpp
@@ -3,6 +3,9 @@
 #include "../../../src/io/RFDataReader.hpp"
 #include "../../../src/io/RFDataReader.hpp"
 
+#include <ortools/linear_solver/linear_solver.h>
+
+
 class GeneralizedRFTest : public testing::Test {
 protected:
 /*Right now an instanciation of test is needed, if we turn it into a free function this needs
@@ -31,11 +34,50 @@ void execute_test(std::string test_file) {
       std::cout << std::endl;
     }
 }
+
+
 };
 /*
 Yes it would be smart to simply loop over an array of strings and call them instead of this repetitive nonsense
 but I would like to have all tests separate
 */
+
+namespace operations_research {
+void BasicExample() {
+  // Create the linear solver with the GLOP backend.
+  std::unique_ptr<MPSolver> solver(MPSolver::CreateSolver("GLOP"));
+
+  // Create the variables x and y.
+  MPVariable* const x = solver->MakeNumVar(0.0, 1, "x");
+  MPVariable* const y = solver->MakeNumVar(0.0, 2, "y");
+
+  LOG(INFO) << "Number of variables = " << solver->NumVariables();
+
+  // Create a linear constraint, 0 <= x + y <= 2.
+  MPConstraint* const ct = solver->MakeRowConstraint(0.0, 2.0, "ct");
+  ct->SetCoefficient(x, 1);
+  ct->SetCoefficient(y, 1);
+
+  LOG(INFO) << "Number of constraints = " << solver->NumConstraints();
+
+  // Create the objective function, 3 * x + y.
+  MPObjective* const objective = solver->MutableObjective();
+  objective->SetCoefficient(x, 3);
+  objective->SetCoefficient(y, 1);
+  objective->SetMaximization();
+
+  solver->Solve();
+
+  LOG(INFO) << "Solution:" << std::endl;
+  LOG(INFO) << "Objective value = " << objective->Value();
+  LOG(INFO) << "x = " << x->solution_value();
+  LOG(INFO) << "y = " << y->solution_value();
+}
+}
+
+TEST_F(GeneralizedRFTest, test_ortools){
+  operations_research::BasicExample();
+}
 TEST_F(GeneralizedRFTest, 24taxa) {
     execute_test("heads/24");
 }
