c3db64c7f1d147ae589568cc0150cfca3897ff96
Robin Andre <uleci@student.kit.edu>
2021-05-26 16:06:14 +0200

    Removed deleted files from git tracking

--- practical-2021/src/Metric.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "PllSplits.hpp"
-class Metrics {
-    public: 
-    virtual double evaluate(const PllSplit& s1, const PllSplit& s2) const = 0;
-    virtual double maximum(const PllSplitList& plist1, const PllSplitList& plist2) const = 0;
-};
