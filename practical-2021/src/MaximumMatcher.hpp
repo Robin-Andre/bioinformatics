58dd747e12f33b065e25355e23813c92b25e1954
Luise <ufziw@student.kit.edu>
2021-06-01 11:23:45 +0200

    Simpler rescaling

--- practical-2021/src/MaximumMatcher.hpp
+++ practical-2021/src/MaximumMatcher.hpp
@@ -3,21 +3,14 @@
 #include <vector>
 #include "ortools/graph/assignment.h"
 #include <utility>
-#include "WeightScaler.hpp"
 
 class MaximumMatcher {
 public:
   static double match(const std::vector<std::vector<double>>& weights) {
     operations_research::SimpleLinearSumAssignment assignment;
-    std::pair<double,double> max_weight = findMax(weights);
-    double min_diff = max_weight.first - max_weight.second;
-    int larger_than_max_weight_int = (int) 2 * max_weight.first;
-    std::vector<std::vector<int64_t>> scaled_weights = WeightScaler::scale(weights);
     for (size_t i = 0; i < weights.size(); ++i) {
       for(size_t j = 0; j < weights[i].size(); ++j) {
-        //TODO the transformation between probabilities and whole numbers recheck. Also is this the integer rounding?
-        //assignment.AddArcWithCost(i, j, (max_weight.first - weights[i][j])/min_diff);
-        assignment.AddArcWithCost(i, j, scaled_weights[i][j]);
+        assignment.AddArcWithCost(i, j, scale(weights[i][j]));
       }
     }
     double result = 0;
@@ -26,10 +19,9 @@ public:
       //printf("The best possible cost is %d.\n", assignment.OptimalCost());
       //printf("An optimal assignment is:\n");
       for (int node = 0; node < assignment.NumNodes(); ++node) {
-        printf("left node %d assigned to right node %d with cost %f (%f).\n",
+        printf("left node %d assigned to right node %d with cost %f.\n",
         node,
         assignment.RightMate(node),
-        (max_weight.first - assignment.AssignmentCost(node)),
         assignment.AssignmentCost(node));
         result += weights[node][assignment.RightMate(node)];
       }
@@ -41,17 +33,9 @@ public:
     return result;
   }
 private:
-  static std::pair<double,double> findMax(const std::vector<std::vector<double>>& weights) {
-    std::pair<double, double> max_weights = std::make_pair(-DBL_MAX, -DBL_MAX);
-    for (size_t i = 0; i < weights.size(); ++i) {
-      for(size_t j = 0; j < weights[i].size(); ++j) {
-        if(weights[i][j] > max_weights.first){
-          max_weights.second = max_weights.first;
-          max_weights.first = weights[i][j];
-        }
-      }
-    }
-    return max_weights;
+  static size_t scale(double weight){
+    double multiplicator = 100;
+    return std::round(-multiplicator*weight);
   }
 
 };
