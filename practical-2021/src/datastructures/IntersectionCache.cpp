908e77865d9ced3b07d0891377b52cc9215724f5
Robin Andre <uleci@student.kit.edu>
2021-07-06 14:38:30 +0200

    Reduced cache build time by 2

--- practical-2021/src/datastructures/IntersectionCache.cpp
+++ practical-2021/src/datastructures/IntersectionCache.cpp
@@ -3,9 +3,14 @@
     IntersectionCache::IntersectionCache(const PllPointerMap& map, const GeneralizedMetric& metric) {
       cache = std::vector(map.size(), std::vector<double>(map.size()));
       for(unsigned i = 0; i < cache.size(); ++i) {
-          //Theoretically we could make this a triangular matrix and make the access well formed i.e. A < B
-          for(unsigned j = 0; j < cache[i].size(); ++j) {
-              cache[i][j] = metric.evaluate(i, j, map);            
+          for(unsigned j = i + 1; j < cache[i].size(); ++j) {
+              cache[i][j] = metric.evaluate(i, j, map);
+              cache[j][i] = cache[i][j];            
           }
+      }
+      for(unsigned i = 0; i < cache.size(); ++i) {
+
+        cache[i][i] = metric.evaluate(i, i, map);           
+          
       }
     }
