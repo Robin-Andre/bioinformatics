a125f872b1d0622792a9bc1b7c11e190c3bd428d
Robin-Andre <82578146+Robin-Andre@users.noreply.github.com>
2021-07-31 16:42:05 +0200

    Delete rename_raxml.sh

--- rename_raxml.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-find ./practical-2021/test/res/reference_results -iname 'RAxML_RF-Distances.0' -execdir mv -i '{}' distances \;
-find ./practical-2021/test/res/reference_results -iname 'RAxML_info.0' -execdir mv -i '{}' info \;
